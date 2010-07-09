1af90ca1aa6e934681fc62127226616a19eb462e
Kwpolska <kwpolska@gmail.com>
2010-07-09 14:39:40 +0200

    creating the branch

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_USR', 'root'); 
-define('DB_PWD', 'password');
-define('DB_NME', 'db'); //database name
-define('DB_TNM', 'kwpastebin'); //table name
-define('DB_HST', 'localhost'); //host
-define('DB_DSN', 'mysql:host='.DB_HST.';dbname='.DB_NME); //using other db? try changing mysql to something else.
-include_once './geshi.php';
-?>
